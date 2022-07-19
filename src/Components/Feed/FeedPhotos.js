03b33d1764a6b7829fee70b11b70387c7c2fef1e
matheusgalote <mateugalote@gmail.com>
2022-07-19 20:18:25 -0300

    Adicionando scroll infinito

--- src/Components/Feed/FeedPhotos.js
+++ src/Components/Feed/FeedPhotos.js
@@ -6,21 +6,25 @@ import Loading from '../../Helper/Loading';
 import { PHOTOS_GET } from '../../api';
 import styles from './FeedPhotos.module.css'
 
-const FeedPhotos = ( {setModalPhoto} ) => {
+const FeedPhotos = ( { page, user, setModalPhoto, setInfinite } ) => {
 
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
 
-    const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 })
-
     const fetchPhotos = async () => {
+      const total = 3;
+      const { url, options } = PHOTOS_GET({ page: page, total: 3, user: user })
       const { resp, json } = await request(url, options);
+
+      if(resp && resp.ok && json.length < total) {
+        setInfinite(false);
+      }
     }
 
     fetchPhotos()
 
-  }, [request]);
+  }, [request, user, setInfinite, page]);
 
   if (error) return <Error error={error} />
   if (loading) return <Loading />;
